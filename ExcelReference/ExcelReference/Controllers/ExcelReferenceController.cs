8455a9e4504301062b21b26c13bd7f0c4a9594c0
yuvaprasath07 <yuvaparasath.visualapp@gmail.com>
2023-08-24 11:35:35 +0530

    Reference Sheet Commit

---
+++ ExcelReference/ExcelReference/Controllers/ExcelReferenceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace ExcelReference.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelReferenceController : ControllerBase
+    {
+        [HttpGet]
+        public string GetExcel()
+        {
+            string filePath = @"D:\Csharpwork\Reference.xlsx";
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage())
+            {
+                var sheet1 = package.Workbook.Worksheets.Add("Sheet1");
+                var sheet2 = package.Workbook.Worksheets.Add("Sheet2");
+                package.SaveAs(new System.IO.FileInfo(filePath));
+            }
+            return "Excel Sheet successfully create";
+        }
+        
+    }
+}
+
